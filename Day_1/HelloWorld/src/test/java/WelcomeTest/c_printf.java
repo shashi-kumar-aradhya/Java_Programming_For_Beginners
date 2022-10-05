923fc6250b7fafe5a2f33777750e5a09e2d79c06
Shashi-Kumar-Aradhya <shashikumararadhya@gmail.com>
2022-10-05 22:12:26 +0530

    c printf

---
+++ Day_1/HelloWorld/src/test/java/WelcomeTest/c_printf.java
@@ -0,0 +1,10 @@
+package WelcomeTest;
+
+public class c_printf {
+
+	public static void main(String[] args) {
+		System.out.printf("Multiplication table %d * %d = %d", 5, 1, 5 * 1).println();
+
+	}
+
+}
